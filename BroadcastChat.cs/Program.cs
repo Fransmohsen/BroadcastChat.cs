ffae1caa01edd068ce7b67672608274220f935ed
Fransmohsen <37370893+Fransmohsen@users.noreply.github.com>
2018-12-18 13:42:48 +0100

    BroadCast
    Chat

--- BroadcastChat.cs/Program.cs
+++ BroadcastChat.cs/Program.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
 
 namespace BroadcastChat.cs
 {
     class Program
     {
+        private const int ListenPort = 11000;
         static void Main(string[] args)
         {
+            //skapar en tråd som körs parallelt 0emed huvudprogrammet
+            var listenThread = new Thread(Listener);
+            listenThread.Start();
+
+            Socket socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Dgram, ProtocolType.Udp);
+
+            socket.EnableBroadcast = true;
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Broadcast, ListenPort);
+
+            while(true)
+            {
+                //Använderen skriver medelande 
+                Console.WriteLine(">");
+                string msg = Console.ReadLine();
+
+                byte[] sendbuf = Encoding.UTF8.GetBytes(msg);
+                socket.SendTo(sendbuf, ep);
+                Thread.Sleep(200);
+            }
+
+
+        }
+        static void Listener()
+        {
+            UdpClient listener = new UdpClient(ListenPort);
+            try
+            {
+                while(true)
+                {
+                    //skapa  objekt som lyssnar efter traik från valfri ip-adress men via port
+                    IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, ListenPort);
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    Console.WriteLine("Mottaget meddelande från {0} : {1}\n",
+                        groupEP.ToString(), Encoding.UTF8.GetString(bytes,0,bytes.Length));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+
         }
     }
 }
